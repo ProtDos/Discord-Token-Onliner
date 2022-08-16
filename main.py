913f5ece3b52570a4bef30d0a93b2a604ead89f7
ProtDos <69071809+ProtDos@users.noreply.github.com>
2022-08-16 13:54:19 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,116 @@
+print("""
+▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄    ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄        ▄               ▄▄▄▄▄▄▄▄▄▄▄  ▄▄        ▄  ▄            ▄▄▄▄▄▄▄▄▄▄▄  ▄▄        ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄ 
+▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌  ▐░▌▐░░░░░░░░░░░▌▐░░▌      ▐░▌             ▐░░░░░░░░░░░▌▐░░▌      ▐░▌▐░▌          ▐░░░░░░░░░░░▌▐░░▌      ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+ ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░▌ ▐░▌ ▐░█▀▀▀▀▀▀▀▀▀ ▐░▌░▌     ▐░▌             ▐░█▀▀▀▀▀▀▀█░▌▐░▌░▌     ▐░▌▐░▌           ▀▀▀▀█░█▀▀▀▀ ▐░▌░▌     ▐░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌
+     ▐░▌     ▐░▌       ▐░▌▐░▌▐░▌  ▐░▌          ▐░▌▐░▌    ▐░▌             ▐░▌       ▐░▌▐░▌▐░▌    ▐░▌▐░▌               ▐░▌     ▐░▌▐░▌    ▐░▌▐░▌          ▐░▌       ▐░▌
+     ▐░▌     ▐░▌       ▐░▌▐░▌░▌   ▐░█▄▄▄▄▄▄▄▄▄ ▐░▌ ▐░▌   ▐░▌ ▄▄▄▄▄▄▄▄▄▄▄ ▐░▌       ▐░▌▐░▌ ▐░▌   ▐░▌▐░▌               ▐░▌     ▐░▌ ▐░▌   ▐░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌
+     ▐░▌     ▐░▌       ▐░▌▐░░▌    ▐░░░░░░░░░░░▌▐░▌  ▐░▌  ▐░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░▌  ▐░▌  ▐░▌▐░▌               ▐░▌     ▐░▌  ▐░▌  ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+     ▐░▌     ▐░▌       ▐░▌▐░▌░▌   ▐░█▀▀▀▀▀▀▀▀▀ ▐░▌   ▐░▌ ▐░▌ ▀▀▀▀▀▀▀▀▀▀▀ ▐░▌       ▐░▌▐░▌   ▐░▌ ▐░▌▐░▌               ▐░▌     ▐░▌   ▐░▌ ▐░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀█░█▀▀ 
+     ▐░▌     ▐░▌       ▐░▌▐░▌▐░▌  ▐░▌          ▐░▌    ▐░▌▐░▌             ▐░▌       ▐░▌▐░▌    ▐░▌▐░▌▐░▌               ▐░▌     ▐░▌    ▐░▌▐░▌▐░▌          ▐░▌     ▐░▌  
+     ▐░▌     ▐░█▄▄▄▄▄▄▄█░▌▐░▌ ▐░▌ ▐░█▄▄▄▄▄▄▄▄▄ ▐░▌     ▐░▐░▌             ▐░█▄▄▄▄▄▄▄█░▌▐░▌     ▐░▐░▌▐░█▄▄▄▄▄▄▄▄▄  ▄▄▄▄█░█▄▄▄▄ ▐░▌     ▐░▐░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░▌      ▐░▌ 
+     ▐░▌     ▐░░░░░░░░░░░▌▐░▌  ▐░▌▐░░░░░░░░░░░▌▐░▌      ▐░░▌             ▐░░░░░░░░░░░▌▐░▌      ▐░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌      ▐░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌
+      ▀       ▀▀▀▀▀▀▀▀▀▀▀  ▀    ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀        ▀▀               ▀▀▀▀▀▀▀▀▀▀▀  ▀        ▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀        ▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀ 
+                                                                                                                                                                    """)
+print("By: @CodingLive#0001")
+print("")
+print("[i] Importing modules...")
+import json
+import random
+import sys
+import threading
+import time
+from concurrent.futures import ThreadPoolExecutor
+import websocket
+from colorama import Fore
+#import pyfade
+
+types = ['Playing', 'Streaming', 'Watching', 'Listening']
+status = ['online', 'dnd', 'idle']
+
+############################################ Change here
+
+GAME = "MY TEXT"  # enter what you want the status to be
+type_ = types[0]  # 0: Playing, 1: Streaming, 2: Watching, 3: Listening
+status = status[0]  # 0: Online, 1: Do Not Disturb, 2: Idle
+
+############################################ Stop changing here
+
+with open("tokens.txt", "r") as f:
+    al = f.read().split("\n")
+    if len(al) <= 1:
+        print(f"{Fore.RED}[!] No tokens found in tokens.txt")
+        exit()
+    else:
+        print(f"{Fore.GREEN}[i] {len(al)} tokens found in tokens.txt")
+
+print("[i] Starting...")
+
+def online(token, game, type, status):
+    ws = websocket.WebSocket()
+    ws.connect('wss://gateway.discord.gg/?v=6&encoding=json')
+    hello = json.loads(ws.recv())
+    heartbeat_interval = hello['d']['heartbeat_interval']
+    if type == "Playing":
+        gamejson = {
+            "name": game,
+            "type": 0
+        }
+    elif type == 'Streaming':
+        gamejson = {
+            "name": game,
+            "type": 1,
+            "url": "https://www.twitch.tv/discord.gg/LilForkTokens"
+        }
+    elif type == "Listening":
+        gamejson = {
+            "name": game,
+            "type": 2
+        }
+    elif type == "Watching":
+        gamejson = {
+            "name": game,
+            "type": 3
+        }
+    else:
+        gamejson = {
+            "name": game,
+            "type": 0
+        }
+
+    auth = {
+        "op": 2,
+        "d": {
+            "token": token,
+            "properties": {
+                "$os": sys.platform,
+                "$browser": "RTB",
+                "$device": f"{sys.platform} Device"
+            },
+            "presence": {
+                "game": gamejson,
+                "status": status,
+                "since": 0,
+                "afk": False
+            }
+        },
+        "s": None,
+        "t": None
+    }
+    ws.send(json.dumps(auth))
+    ack = {
+        "op": 1,
+        "d": None
+    }
+    while True:
+        time.sleep(heartbeat_interval / 1000)
+        try:
+            ws.send(json.dumps(ack))
+            print(f"{Fore.GREEN}[i] {token} is online")
+        except Exception as e:
+            print("[!] Error: " + str(e))
+            break
+
+executor = ThreadPoolExecutor(max_workers=1000)
+for token in open("tokens.txt", "r+").readlines():
+    threading.Thread(target=lambda: online(token.replace("\n", ""), GAME, type_, status)).start()
+print(f"{Fore.GREEN} [+] Tokens are online")
