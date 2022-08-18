2b33bd68e4ae895212fde07cdee0dd88d77a35b1
ProtDos <69071809+ProtDos@users.noreply.github.com>
2022-08-18 15:27:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ import sys
 import threading
 import time
 from concurrent.futures import ThreadPoolExecutor
-import websocket
+import websocket, asyncio
 from colorama import Fore
 #import pyfade
 
@@ -29,15 +29,17 @@ status = ['online', 'dnd', 'idle']
 
 ############################################ Change here
 
-GAME = "MY TEXT"  # enter what you want the status to be
+GAME = "MY TEXT2"  # enter what you want the status to be
 type_ = types[0]  # 0: Playing, 1: Streaming, 2: Watching, 3: Listening
 status = status[0]  # 0: Online, 1: Do Not Disturb, 2: Idle
-stream_url = ""  # URl when choosing "Streaming"
+random_ = True  # True: Random status and type, False: Game status and type
+stream_text = "MY TEXT"  # enter what you want the stream to be
 
 ############################################ Stop changing here
 
 with open("tokens.txt", "r") as f:
     al = f.read().split("\n")
+    #print(len(al))
     if len(al) <= 1:
         if len(al[0]) <= 10:
             print(f"{Fore.RED}[!] No tokens found in tokens.txt")
@@ -49,73 +51,162 @@ with open("tokens.txt", "r") as f:
 
 print("[i] Starting...")
 
+c = 0
+l = len(al)
+
 def online(token, game, type, status):
-    ws = websocket.WebSocket()
-    ws.connect('wss://gateway.discord.gg/?v=6&encoding=json')
-    hello = json.loads(ws.recv())
-    heartbeat_interval = hello['d']['heartbeat_interval']
-    if type == "Playing":
-        gamejson = {
-            "name": game,
-            "type": 0
-        }
-    elif type == 'Streaming':
-        gamejson = {
-            "name": game,
-            "type": 1,
-            "url": stream_url
-        }
-    elif type == "Listening":
-        gamejson = {
-            "name": game,
-            "type": 2
-        }
-    elif type == "Watching":
-        gamejson = {
-            "name": game,
-            "type": 3
-        }
-    else:
-        gamejson = {
-            "name": game,
-            "type": 0
-        }
+    global c
+    global l
+    if random_:
+        type = random.choice(['Playing', 'Streaming', 'Watching', 'Listening', ''])
+        status = ['online', 'dnd', 'idle']
+        status = random.choice(status)
 
-    auth = {
-        "op": 2,
-        "d": {
-            "token": token,
-            "properties": {
-                "$os": sys.platform,
-                "$browser": "RTB",
-                "$device": f"{sys.platform} Device"
-            },
-            "presence": {
-                "game": gamejson,
-                "status": status,
-                "since": 0,
-                "afk": False
+        ws = websocket.WebSocket()
+        ws.connect('wss://gateway.discord.gg/?v=6&encoding=json')
+        hello = json.loads(ws.recv())
+        heartbeat_interval = hello['d']['heartbeat_interval']
+        if type == "Playing":
+            game = random.choice(["Minecraft", "Badlion", "Roblox", "The Elder Scrolls: Online", "DCS World Steam Edit", ])
+            gamejson = {
+                "name": game,
+                "type": 0
             }
-        },
-        "s": None,
-        "t": None
-    }
-    ws.send(json.dumps(auth))
-    ack = {
-        "op": 1,
-        "d": None
-    }
-    while True:
-        time.sleep(heartbeat_interval / 1000)
-        try:
-            print(f"{Fore.GREEN}[i] {token} is online")
-            ws.send(json.dumps(ack))
+        elif type == 'Streaming':
+            gamejson = {
+                "name": game,
+                "type": 1,
+                "url": stream_text
+            }
+        elif type == "Listening":
+            game = random.choice(["Spotify", "Deezer", "Apple Music", "YouTube", "SoundCloud", "Pandora", "Tidal", "Amazon Music", "Google Play Music", "Apple Podcasts", "iTunes", "Beatport"])
+            gamejson = {
+                "name": game,
+                "type": 2
+            }
+        elif type == "Watching":
+            game = random.choice(["YouTube", "Twitch"])
+            gamejson = {
+                "name": game,
+                "type": 3
+            }
+        else:
+            gamejson = {
+                "name": game,
+                "type": 0
+            }
 
-        except Exception as e:
-            print("[!] Error: " + str(e))
-            break
+        auth = {
+            "op": 2,
+            "d": {
+                "token": token,
+                "properties": {
+                    "$os": sys.platform,
+                    "$browser": "RTB",
+                    "$device": f"{sys.platform} Device"
+                },
+                "presence": {
+                    "game": gamejson,
+                    "status": status,
+                    "since": 0,
+                    "afk": False
+                }
+            },
+            "s": None,
+            "t": None
+        }
+        ws.send(json.dumps(auth))
+        ack = {
+            "op": 1,
+            "d": None
+        }
+        while True:
+            time.sleep(heartbeat_interval / 1000)
+            try:
+                c += 1
+                print(f"{Fore.GREEN}[i] {token} is online {c}/{l}")
+                ws.send(json.dumps(ack))
 
-executor = ThreadPoolExecutor(max_workers=1000)
-for token in open("tokens.txt", "r+").readlines():
-    threading.Thread(target=online, args=(token, GAME, type_, status)).start()
+            except Exception as e:
+                print("[!] Error: " + str(e))
+                break
+    else:
+        ws = websocket.WebSocket()
+        ws.connect('wss://gateway.discord.gg/?v=6&encoding=json')
+        hello = json.loads(ws.recv())
+        heartbeat_interval = hello['d']['heartbeat_interval']
+        if type == "Playing":
+            gamejson = {
+                "name": game,
+                "type": 0
+            }
+        elif type == 'Streaming':
+            gamejson = {
+                "name": game,
+                "type": 1,
+                "url": stream_text
+            }
+        elif type == "Listening":
+            gamejson = {
+                "name": game,
+                "type": 2
+            }
+        elif type == "Watching":
+            gamejson = {
+                "name": game,
+                "type": 3
+            }
+        else:
+            gamejson = {
+                "name": game,
+                "type": 0
+            }
+
+        auth = {
+            "op": 2,
+            "d": {
+                "token": token,
+                "properties": {
+                    "$os": sys.platform,
+                    "$browser": "RTB",
+                    "$device": f"{sys.platform} Device"
+                },
+                "presence": {
+                    "game": gamejson,
+                    "status": status,
+                    "since": 0,
+                    "afk": False
+                }
+            },
+            "s": None,
+            "t": None
+        }
+        ws.send(json.dumps(auth))
+        ack = {
+            "op": 1,
+            "d": None
+        }
+        while True:
+            time.sleep(heartbeat_interval / 1000)
+            try:
+                c += 1
+                print(f"{Fore.GREEN}[i] {token} is online {c}/{l}")
+                ws.send(json.dumps(ack))
+
+            except Exception as e:
+                print("[!] Error: " + str(e))
+                break
+
+
+with open("tokens.txt", "r") as f:
+    al = f.read().split("\n")
+
+l = len(al)
+
+
+threads = []
+for i in range(l):
+    t = threading.Thread(target=online, args=(al[i], GAME, type_, status)).start()
+
+
 print(f"{Fore.GREEN} [+] Tokens are online")
